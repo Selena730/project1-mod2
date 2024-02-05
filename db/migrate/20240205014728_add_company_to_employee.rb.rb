01db13acc2cf32a7f21cdf92de9386eb1ee48abc
Ahmad Kayyali <kayyali18@gmail.com>
2024-02-04 21:56:34 -0700

    refactor: add change

--- db/migrate/20240205014728_add_company_to_employee.rb.rb
+++ db/migrate/20240205014728_add_company_to_employee.rb.rb
@@ -1,6 +1,6 @@
 class AddCompanyToEmployee < ActiveRecord::Migration[7.1]
   def change
-    add_column :employees :company_id :integer
+    add_column :employees, :company_id, :integer
     add_index :employees, :company_id
     add_foreign_key :employees, :companies
   end
